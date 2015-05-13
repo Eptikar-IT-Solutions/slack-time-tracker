c9cf9ee6212d2530ad23eb51453b3c20086b0b55
Mohamed Nimir <mohamed.nimir@eptikar.com>
2015-05-13 07:14:04 +0300

    fixed mongodb date compare issue

--- collections/days.js
+++ collections/days.js
@@ -41,7 +41,7 @@ daySchema = new SimpleSchema({
   },
 
   date: {
-    type: Date
+    type: String
   },
 
   sprints: {
@@ -55,22 +55,32 @@ Days.attachSchema(daySchema);
 // Methods //
 Meteor.methods({
   addCheckIn: function(userId, checkIn) {
+    date = '' + checkIn.getFullYear() + checkIn.getMonth() + checkIn.getDate();
     return Days.update(
-      { userId: userId, date: checkIn.getDate() },
+      { 
+        userId: userId, 
+        date: date 
+      },
       {
         $addToSet: { sprints: { checkIn: checkIn } },
-        $setOnInsert: { userId: userId, date: checkIn.getDate(), $push: {sprints: { checkIn: checkIn }} }
+        $setOnInsert: { userId: userId, date: date, $push: {sprints: { checkIn: checkIn }} }
       },
       { upsert: true }
     ); 
   },
 
   addCheckOut: function(userId, checkOut) {
+    date = '' + checkOut.getFullYear() + checkOut.getMonth() + checkOut.getDate();
     return Days.update(
-      { userId: userId, date: checkOut.getDate() , sprints: 
-        { $elemMatch: {checkIn: { $exists: true}, checkOut: { $exists: false} } } 
+      { 
+        userId: userId, 
+        date: date, 
+        sprints: { $elemMatch: { checkIn: { $exists: true}, checkOut: { $exists: false} } } 
       },
-      { $set: { 'sprints.$.checkOut': checkOut } });    
+      { 
+        $set: { 'sprints.$.checkOut': checkOut } 
+      } 
+    );    
   }
 });
 
@@ -79,6 +89,19 @@ Meteor.startup(function() {
   if (Meteor.isServer && Days.find().count() === 0) {
     Meteor.call('pull_times', function(error, messages) {
       if(error) { new Meteor.throw(error); return }
+      // CHECKIN
+      messages.forEach(function(message, index) {
+        if(!message.hasOwnProperty('subtype')) {
+          var userId = Meteor.users.findOne({'profile.slackId': message.user})._id;
+          var time = new Date(message.ts * 1000);
+
+          if (message.text.trim() === '1') {
+            console.log('DEBUGGING CHECKIN', userId, time);
+            Meteor.call('addCheckIn', userId, time);
+          }
+        }
+      });
+      // CHECKOUT
       messages.forEach(function(message, index) {
         if(!message.hasOwnProperty('subtype')) {
           var userId = Meteor.users.findOne({'profile.slackId': message.user})._id;
@@ -88,13 +111,9 @@ Meteor.startup(function() {
             console.log('DEBUGGING CHECKOUT', userId, time);
             Meteor.call('addCheckOut', userId, time);
           }
-
-          if (message.text.trim() === '1') {
-            console.log('DEBUGGING CHECKIN', userId, time);
-            Meteor.call('addCheckIn', userId, time);
-          }
         }
       });
     });
   };
 });
+
